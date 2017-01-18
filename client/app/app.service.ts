44710d09ea62973d597ab0939e4b9f70b226518a
elaijuh <hjiale@gmail.com>
2017-01-18 16:57:48 +0800

    update angular to 2.4.3 and fix login check logic

--- client/app/app.service.ts
+++ client/app/app.service.ts
@@ -62,12 +62,21 @@ export class SocketService {
  * AuthGuard service is to provide client side router authorization
  */
 export class AuthGuard implements CanActivate {
-    constructor(private _socketService: SocketService, private _router: Router) {}
+  constructor(private _socketService: SocketService, private _router: Router) { }
 
-    canActivate() {
-        if (this._socketService.isLogin()) return true
-        this._router.navigate(['/'])
-        return false
-    }
+  canActivate() {
+    // if (this._socketService.isLogin()) return true
+    // this._router.navigate(['/'])
+    // return false
+    return this._socketService.isLogin() || this._socketService.authenticate()
+      .then(res => this._socketService.app.passport.verifyJWT(res.accessToken))
+      .then(payload => this._socketService.app.service('users').get(payload.userId))
+      .then(user => this._socketService.app.set('user', user))
+      .then(res => true)
+      .catch(err => {
+        this._router.navigate(['/login']
+        )
+      })
+  }
 }
 
